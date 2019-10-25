9f7bd0c5f0e839c3601bc32aba8947ca0b5adf1d
Ivan Kuznetsov <me@jeiwan.ru>
2019-10-25 15:06:10 +0700

    Implement 'binary' package

---
+++ binary/binary.go
@@ -0,0 +1,103 @@
+package binary
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"reflect"
+)
+
+const (
+	commandLength          = 12
+	magicAndChecksumLength = 4
+)
+
+// Marshaler is the interface implemented by types that can marshal themselves into binary.
+type Marshaler interface {
+	MarshalBinary() ([]byte, error)
+}
+
+// Marshal returns the binary encoding of v.
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+
+	switch v.(type) {
+	case int32, uint32, int64, uint64, bool:
+		if err := binary.Write(&buf, binary.LittleEndian, v); err != nil {
+			return nil, err
+		}
+
+	// port
+	case uint16:
+		if err := binary.Write(&buf, binary.BigEndian, v); err != nil {
+			return nil, err
+		}
+
+	case [magicAndChecksumLength]byte:
+		magic, ok := v.([magicAndChecksumLength]byte)
+		if !ok {
+			return nil, fmt.Errorf("invalid magic or checksum: %v", v)
+		}
+
+		if _, err := buf.Write(magic[:]); err != nil {
+			return nil, err
+		}
+
+	case [commandLength]byte:
+		command, ok := v.([commandLength]byte)
+		if !ok {
+			return nil, fmt.Errorf("invalid command: %v", v)
+		}
+
+		if _, err := buf.Write(command[:]); err != nil {
+			return nil, err
+		}
+
+	case []byte:
+		bytes, ok := v.([]byte)
+		if !ok {
+			return nil, fmt.Errorf("invalid byte array: %v", v)
+		}
+
+		if _, err := buf.Write(bytes); err != nil {
+			return nil, err
+		}
+
+	case Marshaler:
+		return v.(Marshaler).MarshalBinary()
+
+	default:
+		// is it a struct?
+		if reflect.ValueOf(v).Kind() == reflect.Struct {
+			return marshalStruct(v)
+		}
+
+		return nil, fmt.Errorf("unsupported type %s", reflect.TypeOf(v).String())
+	}
+
+	return buf.Bytes(), nil
+}
+
+func marshalStruct(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	vv := reflect.ValueOf(v)
+
+	for i := 0; i < vv.NumField(); i++ {
+		s, err := Marshal(reflect.Indirect(vv.Field(i)).Interface())
+		if err != nil {
+			f := reflect.TypeOf(v).Field(i).Name
+			return nil, fmt.Errorf("failed to marshal field %s: %v", f, err)
+		}
+
+		if _, err := buf.Write(s); err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
+}
+
+// Unmarshal parses the binary-encoded data and stores the result in the value pointed to by v.
+func Unmarshal(data []byte, v interface{}) error {
+	return nil
+}
---
+++ binary/binary_test.go
@@ -0,0 +1,138 @@
+package binary_test
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/Jeiwan/tinybit/binary"
+)
+
+type customType []byte
+
+func (ct customType) MarshalBinary() ([]byte, error) {
+	return []byte{0xde, 0xad, 0xbe, 0xef}, nil
+}
+
+func TestMarshal(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    interface{}
+		err      error
+		expected []byte
+	}{
+		{name: "int32",
+			input:    int32(1337),
+			err:      nil,
+			expected: []byte{0x39, 0x05, 0x00, 0x00}},
+
+		{name: "uint32",
+			input:    uint32(1337),
+			err:      nil,
+			expected: []byte{0x39, 0x05, 0x00, 0x00}},
+
+		{name: "int64",
+			input:    int64(1337),
+			err:      nil,
+			expected: []byte{0x39, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+
+		{name: "uint64",
+			input:    int64(1337),
+			err:      nil,
+			expected: []byte{0x39, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+
+		{name: "bool true",
+			input:    true,
+			err:      nil,
+			expected: []byte{0x01}},
+
+		{name: "bool false",
+			input:    false,
+			err:      nil,
+			expected: []byte{0x00}},
+
+		{name: "magic or checksum",
+			input:    [4]byte{0x31, 0x33, 0x70, 0x00},
+			err:      nil,
+			expected: []byte{0x31, 0x33, 0x70, 0x00}},
+
+		{name: "command",
+			input:    [12]byte{0x74, 0x65, 0x73, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+			err:      nil,
+			expected: []byte{0x74, 0x65, 0x73, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+
+		{name: "byte array",
+			input:    []byte{0x12, 0x34, 0x56, 0x78},
+			err:      nil,
+			expected: []byte{0x12, 0x34, 0x56, 0x78}},
+
+		{name: "struct",
+			input: struct {
+				Test  uint32
+				Magic [4]byte
+				Data  []byte
+			}{
+				Test:  31337,
+				Magic: [4]byte{0x12, 0x34, 0x56, 0x78},
+				Data:  []byte{0xde, 0xad, 0xbe, 0xef}},
+			err:      nil,
+			expected: []byte{0x69, 0x7A, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0xde, 0xad, 0xbe, 0xef}},
+
+		{name: "struct with a pointer",
+			input: struct {
+				Test    uint32
+				Pointer *struct {
+					Test uint32
+				}
+			}{
+				Test: 31337,
+				Pointer: &struct {
+					Test uint32
+				}{
+					Test: 31337,
+				}},
+			err:      nil,
+			expected: []byte{0x69, 0x7A, 0x00, 0x00, 0x69, 0x7A, 0x00, 0x00}},
+
+		{name: "struct with a string",
+			input: struct {
+				Test   uint32
+				String string
+			}{
+				Test:   31337,
+				String: "test"},
+			err:      errors.New("failed to marshal field String: unsupported type string"),
+			expected: []byte{}},
+
+		{name: "custom marshaler",
+			input:    customType{},
+			err:      nil,
+			expected: []byte{0xde, 0xad, 0xbe, 0xef}},
+
+		{name: "unsupported type",
+			input:    [3]byte{0x12, 0x34, 0x56},
+			err:      errors.New("unsupported type [3]uint8"),
+			expected: nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
+			actual, err := binary.Marshal(test.input)
+			if err != nil && test.err != nil {
+				if test.err.Error() != err.Error() {
+					tt.Errorf("expected error: %v, actual: %v", test.err, err)
+				}
+				return
+			}
+
+			if err != nil && test.err == nil {
+				tt.Errorf("didn't expect an error: %v", err)
+				return
+			}
+
+			if !bytes.Equal(actual, test.expected) {
+				tt.Errorf("expected: %x, actual %x", test.expected, actual)
+			}
+		})
+	}
+}
