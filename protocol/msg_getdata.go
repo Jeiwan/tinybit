54e8d42b6ca910ae6f8fe2e39510ac63091dda3c
Ivan Kuznetsov <me@jeiwan.ru>
2019-12-02 18:56:29 +0700

    Custom marshaling for 'getdata' and 'inv' messages

--- protocol/msg_getdata.go
+++ protocol/msg_getdata.go
@@ -1,7 +1,40 @@
 package protocol
 
+import (
+	"bytes"
+
+	"github.com/Jeiwan/tinybit/binary"
+)
+
 // MsgGetData represents 'getdata' message.
 type MsgGetData struct {
 	Count     uint8 // TODO: Change to var_int
 	Inventory []InvVector
+}
+
+// MarshalBinary implements binary.Marshaler interface.
+func (gd MsgGetData) MarshalBinary() ([]byte, error) {
+	buf := bytes.NewBuffer([]byte{})
+
+	b, err := binary.Marshal(gd.Count)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := buf.Write(b); err != nil {
+		return nil, err
+	}
+
+	for _, i := range gd.Inventory {
+		b, err := binary.Marshal(i)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := buf.Write(b); err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
 }
--- protocol/msg_inv.go
+++ protocol/msg_inv.go
@@ -1,5 +1,11 @@
 package protocol
 
+import (
+	"io"
+
+	"github.com/Jeiwan/tinybit/binary"
+)
+
 const (
 	DataObjectError = iota
 	DataObjectTx
@@ -12,6 +18,27 @@ const (
 type MsgInv struct {
 	Count     uint8 // TODO: Change to var_int
 	Inventory []InvVector
+}
+
+// UnmarshalBinary implements binary.Unmarshaler interface.
+func (inv *MsgInv) UnmarshalBinary(r io.Reader) error {
+	d := binary.NewDecoder(r)
+
+	if err := d.Decode(&inv.Count); err != nil {
+		return err
+	}
+
+	for i := uint8(0); i < inv.Count; i++ {
+		var v InvVector
+
+		if err := d.Decode(&v); err != nil {
+			return err
+		}
+
+		inv.Inventory = append(inv.Inventory, v)
+	}
+
+	return nil
 }
 
 // InvVector represents inventory vector.
